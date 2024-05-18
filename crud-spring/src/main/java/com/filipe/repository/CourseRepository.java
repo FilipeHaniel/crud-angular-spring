a31c5ef5a5edec4524c12267d11b23bb8de6a380
Filipe Haniel <filipe.haniel@gmail.com>
2024-05-17 21:24:09 -0300

    build backend

---
+++ crud-spring/src/main/java/com/filipe/repository/CourseRepository.java
@@ -0,0 +1,9 @@
+package com.filipe.repository;
+
+import com.filipe.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+}
